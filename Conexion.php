71c612abb6984a0377c57a127545f16718cbe8a2
JessAndMx <147351988+JessAndMx@users.noreply.github.com>
2023-10-08 17:41:17 -0600

    Initial commit

---
+++ Conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+    $host = "localhost";
+    $User = "root";
+    $pass = "";
+    $db = "iniciosesiondb";
+
+    $conexion = mysqli_connect($host, $User , $pass, $db);
+
+    if (!$con) {
+     echo "Conexion fallida";
+    }
